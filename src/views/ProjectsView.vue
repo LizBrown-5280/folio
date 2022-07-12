63b2dcab9646ab458dd58d0bd400d6226cb21b2d
Liz Brown <lizbrown5280@gmail.com>
2022-07-12 15:28:36 -0600

    projects page start

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Projects Page</h1>
+  <router-link :to="{ name: 'MenuView' }"><h2>Summer BBQ Menu</h2></router-link>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ProjectsView',
+};
+</script>
+
+<style></style>
