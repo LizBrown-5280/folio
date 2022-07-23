d65dbc143c0660468979ee35756726f54acf6913
Liz Brown <lizbrown5280@gmail.com>
2022-07-23 11:50:57 -0600

    updated Vuex store to contain foodMenu module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { createStore } from 'vuex';
+import * as foodMenu from './modules/foodMenu';
 
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    foodMenu,
+  },
 });
